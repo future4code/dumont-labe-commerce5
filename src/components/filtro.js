cdadb80eaee126d2239c6106ce169ea7177a3bcf
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-21 18:33:49 -0300

    esqueleto do projeto - componentes e app.js

---
+++ src/components/filtro.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+class Filtro extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Filtros</h1>
+            </div>
+        )
+    }
+}
+
+export default Filtro;
