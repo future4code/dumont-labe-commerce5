4cef4f7982decc8d0d0d2728469e38acadd86617
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-10-22 00:24:48 -0300

    Componente carrinho.js alterado

--- src/components/carrinho.js
+++ src/components/carrinho.js
@@ -1,13 +1,42 @@
-import react from 'react';
+import React from 'react';
+import styled from "styled-components";
+
+import CarrinhoItem from "../CarrinhoItem";
+
+const CarrinhoContainer = styled.div`
+  border: 1px solid black;
+`;
+
+const ListaProdutosContainer = styled.div`
+  display: grid;
+  gap: 4px;
+`;
+
+//Para somar o total de itens no carrinho
+// getTotalValue = () => {
+//   let totalValue = 0;
+
+//   for (let produto of this.props.produtosCarrinho) {
+//     totalValue += produto.price * produto.quality;
+//   }
+//   return totalValue;
+// };
 
 class Carrinho extends React.Component {
-    render(){
-        return(
-            <div>
-                <h1>Carrinho</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <CarrinhoContainer>
+        <h3>Carrinho:</h3>
+        <ListaProdutosContainer>
+          {/* {this.props.produtosCarrinho.map((produto) => {
+            return <CarrinhoItem cartItem={produto}/>
+          })} TESTE*/}
+          <CarrinhoItem />
+          <p>Valor total:</p>
+        </ListaProdutosContainer>
+      </CarrinhoContainer>
+    );
+  }
 }
 
-export default Carrinho 
+export default Carrinho;
