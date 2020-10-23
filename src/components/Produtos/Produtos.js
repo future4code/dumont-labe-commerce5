0cfcffc4845d59eec2e7c21b456f1fb492e642cd
EvelynD95 <evelyn.dantas95@gmail.com>
2020-10-23 14:14:30 -0300

    Tentativa de debugging - Erro instance.render

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CardProdutos } from "./CardProdutos";
+import CardProdutos from "./CardProdutos";
 import styled from "styled-components";
 
 const ProdutosContainer = styled.div`
@@ -17,9 +17,8 @@ const ProdutosGrid = styled.div `
   gap: 16px;
   padding: 16px;
 `
-//Resolvi já colocar a função de ordenação aqui (apagar esse comentário) 
 
-export class Produtos extends React.Component {
+export default class Produtos extends React.Component {
   pegarOrdenarEFiltrarLista = () => {
     return this.props.produtos
       .filter((produto) => produto.preco < this.props.filtroMinimo)
@@ -60,4 +59,3 @@ export class Produtos extends React.Component {
     );
   }
 }
-export default Produtos;
