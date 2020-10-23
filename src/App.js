7f069b750a96f1a63d2af9a523fc76cb39d89586
Beatriz Cupolillo <beatrizcupolillo@gmail.com>
2020-10-22 21:12:56 -0300

    refatorado Produtos/Grid e App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import Cards from './components/cards.js';
-import Carrinho from './components/carrinho.js';
-import Filtro from './components/filtro.js';
+import React from "react";
+import Filtro from "./components/Filtro/filtro";
+import Produtos from "./components/Produtos/Produtos"
+import Carrinho from "./components/carrinho";
 import styled from 'styled-components';
 
-const ProdutoWrapper = styled.div`
+const AppContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 5px;
-  border: 1px solid black;
-  `
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
 
-function App() {
+const produtos = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/jWBSVYz/camiseta-star-wars-1.jpg",
+    nome: "Camiseta Star Wars",
+    preco: 50,
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/Q8ZG3jN/camiseta-naza-meme.jpg",
+    nome: "Produto 2",
+    preco: 50,
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/v3t36dK/camiseta-nasa-1.jpg",
+    nome: "Produto 3",
+    preco: 50,
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/gmZ13qQ/camiseta-et-1.jpg",
+    nome: "Produto 4",
+    preco: 50,
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/M6B4SYR/camiseta-espaco-2.jpg",
+    nome: "Produto 5",
+    preco: 50,
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/G9Wr6TK/camiseta-espaco.jpg",
+    nome: "Produto 6",
+    preco: 50,
+  },
+  {
+    id: 7,
+    img: "https://i.ibb.co/hXK2krh/camiseta-dog-space.jpg",
+    nome: "Produto 7",
+    preco: 50,
+  },
+  {
+    id: 8,
+    img: "https://i.ibb.co/7v7GxDs/camiseta-cat-galaxy.jpg",
+    nome: "Produto 8",
+    preco: 50,
+  },
+];
+
+export default function App() {
   return (
-    <div>
-      <ProdutoWrapper>
-        <Filtro/>
-        <Cards/>
-        <Carrinho/>
-      </ProdutoWrapper>
-    </div>
+    <AppContainer>
+      <Filtro />
+      <Produtos produtos={produtos} />
+      <Carrinho />
+    </AppContainer>
   );
 }
-
-export default App;
---
+++ src/components/Produtos/CardProdutos.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { Produtos } from "./Produtos";
+
+const CardContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CardInfo = styled.div`
+display: flex;
+flex-direction:column;
+padding: 16px;
+margin-top: 5px;
+
+p {
+  margin: 5px 0;
+}
+`
+const BotaoAddCarrinho = styled.button`
+  align-self: center;
+`
+
+export class CardProdutos extends React.Component {
+  render (){
+    const produto = this.props.produto
+    return <CardContainer>
+    <img src={produto.img}/>
+      <CardInfo>
+        <p>{produto.nome}</p>
+        <p>R${produto.preco},00 </p>
+        <BotaoAddCarrinho> Adicionar ao carrinho </BotaoAddCarrinho>
+      </CardInfo>
+    </CardContainer>
+  }
+}
+
+export default CardProdutos;
